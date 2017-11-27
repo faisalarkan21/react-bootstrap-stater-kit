4603ff688fcd6fa32df80595c1783c29da35d3c6
faisal arkan <faisalarkan21@gmail.com>
2017-11-27 07:06:39 +0700

    add navbar

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import store from './src/store/configureStore';
 import Root from './src/containers/App';
 
+// eslint-disable-next-line
+import 'typeface-roboto'; 
 import './public/style/base.scss';
 
 ReactDOM.render(
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import { Panel, Col, Button } from 'react-bootstrap';
+import { Panel, Col, Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class Dashboard extends Component {
   render() {
     return (
       <div className >
-        <Col xs={4} md={5} lg={4} >
+        <Navbar className="navbar navbar-default navbar-fixed-top">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Bootstrap Paper</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Navbar.Form pullRight>
+            <Button bsStyle="primary" >Login / Sign up</Button>
+          </Navbar.Form>
+        </Navbar>
+
+        <Col id="post-panel" xs={4} md={5} lg={4} >
           <Panel header="ReadMe" bsStyle="primary">
             Hi there, i'm just making webpack config and basic example component
             <code>bootstrap paper with react and redux stuff</code>,
